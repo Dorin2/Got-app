7315c6e4e4932e3e575019b0c75536353b5eae55
Dorin <dorin.ghetus98@gmail.com>
2022-08-13 14:58:52 +0300

    C7

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './spinner.css'
+
+
+const Spinner =() =>{
+    return(
+        <div class="loadingio-spinner-reload-amk30vbzi1m"><div class="ldio-r18sd0xlk6c">
+            <div><div></div><div></div><div></div></div></div>
+        </div>
+    )
+}
+
+export default Spinner;
